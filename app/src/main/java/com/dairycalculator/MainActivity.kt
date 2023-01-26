e6a45f2c974ac00104dae6a4c5477d6e8fabbfa5
Rajwardhan Sagare <rajsagare14@gmail.com>
2023-01-25 23:17:04 -0800

    XML complete commit

--- app/src/main/java/com/dairycalculator/MainActivity.kt
+++ app/src/main/java/com/dairycalculator/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.dairycalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.dairycalculator.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val snfCalculator = findViewById<Button>(R.id.snfCalculator)
+        snfCalculator.setOnClickListener(){
+            val intent = Intent(this,SNFCalculator::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/dairycalculator/SNFCalculator.kt
@@ -0,0 +1,12 @@
+package com.dairycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dairycalculator.R
+
+class SNFCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snfcalculator)
+    }
+}
