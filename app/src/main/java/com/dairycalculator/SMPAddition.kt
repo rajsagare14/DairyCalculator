09cdfa18bf67223952c8b946827b702b5c8c9362
Rajwardhan Sagare <rajsagare14@gmail.com>
2023-02-08 21:49:47 +0530

    smp Addition

---
+++ app/src/main/java/com/dairycalculator/SMPAddition.kt
@@ -0,0 +1,11 @@
+package com.dairycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SMPAddition : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smpaddition)
+    }
+}
