bf02f7e72ba91aa0f8e6386e1a95be7ec5b38074
Rajwardhan Sagare <rajsagare14@gmail.com>
2023-03-02 20:51:12 +0530

    Cream Production

---
+++ app/src/main/java/com/dairycalculator/SMPProduction.kt
@@ -0,0 +1,12 @@
+package com.dairycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SMPProduction : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smpproduction)
+    }
+}
